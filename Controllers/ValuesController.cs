4b00c24a69637462720cd8e30a525fba5c20ef5d
DSL 1 <sojibhossain653@gmail.com>
2024-05-05 16:21:23 +0600

    updated other functionality

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
+using Newtonsoft.Json;
 
 namespace CRUDusingSP.Controllers
 {
@@ -16,18 +17,6 @@ namespace CRUDusingSP.Controllers
 		Employee emp = new Employee();
 		// GET: Values
 		public ActionResult Index()
-		{
-			return View();
-		}
-
-		// GET: Values/Details/5
-		public ActionResult Details(int id)
-		{
-			return View();
-		}
-
-		// GET: Values/Create
-		public ActionResult Create()
 		{
 			return View();
 		}
@@ -59,48 +48,115 @@ namespace CRUDusingSP.Controllers
 			return msg;
 		}
 
+		// GET: Values/GetAllEmployee
+		public dynamic GetAllEmployee()
+		{
+			SqlDataAdapter da = new SqlDataAdapter("usp_GetAllEmployee", con);
+			da.SelectCommand.CommandType = CommandType.StoredProcedure;
+			DataTable dt = new DataTable();
+			da.Fill(dt);
+			List<Employee> listEmployee = new List<Employee>();
+			if (dt.Rows.Count > 0)
+			{
+				for (int i = 0; i < dt.Rows.Count; i++)
+				{
+					Employee emp = new Employee();
+					emp.Name = dt.Rows[i]["Name"].ToString();
+					emp.Id = Convert.ToInt32(dt.Rows[i]["ID"]);
+					emp.Age = Convert.ToInt32(dt.Rows[i]["Age"]);
+					emp.Active = Convert.ToInt32(dt.Rows[i]["Active"]);
+					listEmployee.Add(emp);
+				}
+			}
+			if (listEmployee.Count > 0)
+			{
+				return JsonConvert.SerializeObject(listEmployee);
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+
+		// GET: Values/GetEmployeeByID
+		public dynamic GetEmployeeByID(int Id)
+		{
+			SqlDataAdapter da = new SqlDataAdapter("usp_GetEmployeeByID", con);
+			da.SelectCommand.CommandType = CommandType.StoredProcedure;
+			da.SelectCommand.Parameters.AddWithValue("@Id", Id);
+			DataTable dt = new DataTable();
+			da.Fill(dt);
+			Employee emp = new Employee();
+			if (dt.Rows.Count > 0)
+			{
+				emp.Name = dt.Rows[0]["Name"].ToString();
+				emp.Id = Convert.ToInt32(dt.Rows[0]["ID"]);
+				emp.Age = Convert.ToInt32(dt.Rows[0]["Age"]);
+				emp.Active = Convert.ToInt32(dt.Rows[0]["Active"]);
+
+			}
+			if (emp != null)
+			{
+				return JsonConvert.SerializeObject(emp);
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+
 		// GET: Values/Edit/5
-		public ActionResult Edit(int id)
-		{
-			return View();
-		}
-
-		// POST: Values/Edit/5
 		[HttpPost]
-		public ActionResult Edit(int id, FormCollection collection)
+		public string Edit(int Id, Employee employee)
 		{
-			try
+			string msg = "";
+			if (employee != null)
 			{
-				// TODO: Add update logic here
-
-				return RedirectToAction("Index");
+				SqlCommand cmd = new SqlCommand("usp_UpdateEmployee", con);
+				cmd.CommandType = CommandType.StoredProcedure;
+				cmd.Parameters.AddWithValue("@Id", Id);
+				cmd.Parameters.AddWithValue("@Name", employee.Name);
+				cmd.Parameters.AddWithValue("@Age", employee.Age);
+				cmd.Parameters.AddWithValue("@Active", employee.Active);
+				con.Open();
+				int i = cmd.ExecuteNonQuery();
+				con.Close();
+				if (i > 0)
+				{
+					msg = "Data Updated Successfully";
+				}
+				else
+				{
+					msg = "Error";
+				}
 			}
-			catch
-			{
-				return View();
-			}
+			return msg;
 		}
+
 
 		// GET: Values/Delete/5
-		public ActionResult Delete(int id)
+		public string Delete(int Id)
 		{
-			return View();
+			string msg = "";
+			SqlCommand cmd = new SqlCommand("usp_DeleteEmployee", con);
+			cmd.CommandType = CommandType.StoredProcedure;
+			cmd.Parameters.AddWithValue("@Id", Id);
+			con.Open();
+			int i = cmd.ExecuteNonQuery();
+			con.Close();
+			if (i > 0)
+			{
+				msg = "Data Deleted Successfully";
+			}
+			else
+			{
+				msg = "Error";
+			}
+			return msg;
 		}
 
-		// POST: Values/Delete/5
-		[HttpPost]
-		public ActionResult Delete(int id, FormCollection collection)
-		{
-			try
-			{
-				// TODO: Add delete logic here
 
-				return RedirectToAction("Index");
-			}
-			catch
-			{
-				return View();
-			}
-		}
 	}
 }
